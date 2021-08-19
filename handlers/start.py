e3f992c3dbd1cb38016772bd35a8454f7ae35d4d
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-19 19:20:04 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -7,66 +7,66 @@ from config import BOT_USERNAME
 @Client.on_message(filters.command(["start", "start@GroupMusicPlayBot"]) & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        text="**Hello 👋🏻 {}!**\n\nI **Can Play Music In Voice Chats of Telegram Groups.**I Have A **lot of cool feature that will amaze You!**\n\n**Click /cmdlist For More Help On My Usage ❤**".format(message.from_user.mention),
+        text="**🎈Hello 👋🏻 {}!**\n\n 🤡I Am Simple Vc Music Play Bot🤗 💥I **Can Play Music In Voice Chats of Telegram Groups.**🥶I Have A **lot of cool feature that will amaze You🥶!**\n\n**🤗send /cmdlist For More Help On My Usage ❤  🤗This Bot Made By @omindas 🤗**".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/GroupMuSicPlayBot?startgroup=true")
+            InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/SD_VC_Music_Play_Ro_Bot?startgroup=true")
             ],[
-            InlineKeyboardButton("💬 Group", url="https://t.me/MusicBotSupports"),
-            InlineKeyboardButton("Channel 🔊", url="https://t.me/GroupMusicXNews")
+            InlineKeyboardButton("💬 Group", url="https://t.me/sdbotworld"),
+            InlineKeyboardButton("Channel 🔊", url="https://t.me/sdprojectupdates")
             ],[
-            InlineKeyboardButton("Commands 🛠", url="https://telegra.ph/Music-Bot-05-07")
+            InlineKeyboardButton("🎲Commands 🛠", url="https://telegra.ph/Music-Bot-05-07")
             ]]
         ),
         disable_web_page_preview=True
     )
         
-@Client.on_message(filters.command(["start", "start@GroupMusicPlayBot"]) & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", "start@SD_VC_Music_Play_Ro_Bot"]) & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
     await message.reply_text(
         text="**Music Bot Is Online ✅**",
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
+            InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/sdbotworld")
             ]]
         )
     )
 
 
-@Client.on_message(filters.command(["cmdlist", "start@GroupMusicPlayBot"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["cmdlist", "start@SD_VC_Music_Play_Ro_Bot"]) & filters.private & ~filters.channel)
 async def cmdlist(_, message: Message):
     await message.reply_text(
-        text="""**Group Music Bot : Help Menu**
+        text="""**🤗Welcome To : Help Menu❤️**
 
-__× First Add Me To Your Group..
-× Promote Me As Admin In Your Group With All Permission..__
+__× 👉First Add Me To Your Group..👈
+× 👉Promote Me As Admin In Your Group With All Permission..👈__
 
 **🏷 Common Commands.**
 
-• `/play` - Song Name : __Plays Via Youtube__
-• `/dplay` - Song Name : __Play Via Deezer__
-• `/splay` - Song Name : __Play Via Jio Saavn__
-• `/playlist` - __Show now playing list__
-• `/current` - __Show now playing__
+• `/play` - Song Name : __Plays Via Youtube🎈__
+• `/dplay` - Song Name : __Play Via Deezer🎈__
+• `/splay` - Song Name : __Play Via Jio Saavn🎈__
+• `/playlist` - __Show now playing list🎈__
+• `/current` - __Show now playing🎈__
 
-• `/song` - Song Name : __Get The Song From YouTube__
-• `/vid` - Video Name : __Get The Video From YouTube__
-• `/deezer` - song name : __download songs you want quickly via deezer__
-• `/saavn` - song name : __download songs you want quickly via saavn__
-• `/search` - YouTube Title : __(Get YouTube Search Query)__
+• `/song` - Song Name : __Get The Song From YouTube🎈__
+• `/vid` - Video Name : __Get The Video From YouTube🎈__
+• `/deezer` - song name : __download songs you want quickly via deezer🎈__
+• `/saavn` - song name : __download songs you want quickly via saavn🎈__
+• `/search` - YouTube Title : __(Get YouTube Search Query)🎈__
 
 **🏷 Group Admin Commands.**
 
-• `/skip` : __Skips Music__
-• `/pause` : __Pause Playing Music__
-• `/resume` : __Resume Playing Music__
-• `/end` : __Stops playing Music__
-• `/reload` : __Reloads Admin List__
-• `/userbotjoin` : __Assistant Joins The Group__
-• `/userbotleave` : __Assistant Leaves From The Group.__""",
+• `/skip` : __Skips Music👨‍💻__
+• `/pause` : __Pause Playing Music👨‍💻__
+• `/resume` : __Resume Playing Music👨‍💻__
+• `/end` : __Stops playing Music👨‍💻__
+• `/reload` : __Reloads Admin List👨‍💻__
+• `/userbotjoin` : __Assistant Joins The Group👨‍💻__
+• `/userbotleave` : __Assistant Leaves From The Group👨‍💻.__""",
         reply_markup=InlineKeyboardMarkup(
               [[
-              InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
+              InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/sdbotworld")
               ]]
           )
       )
