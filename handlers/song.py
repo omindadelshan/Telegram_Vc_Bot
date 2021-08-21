598b7290482eceb662a8ffc284f2a36df26acce0
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-21 18:02:49 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -31,7 +31,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply('🔎 Searching Song')
     ydl_opts = {"format": "bestaudio/best"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -54,13 +54,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("Downloading the song ")
+    m.edit("📤Uploading song... ")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @GroupMusicPlayBot **'
+        rep = '**🎵 Uploaded by @SD_VC_Music_Play_Ro_Bot **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
